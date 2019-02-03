502c7f3b2e0ed80c531c82a4fe03916cf84e86ec
onlymisaky <onlymisaky@xingren.com>
2019-02-04 01:59:22 +0800

    routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,17 +1,8 @@
-import {
-  Ng1StateDeclaration,
-  Transition,
-  StateDeclaration
-} from '@uirouter/angularjs';
-
-import * as angular from 'angular';
-
-import { ILazyLoad } from 'oclazyload';
-
+import { Ng1StateDeclaration } from '@uirouter/angularjs';
 
 export const routes: Ng1StateDeclaration[] = [
   { name: 'index', url: '', redirectTo: { state: 'dashboard' } },
   { name: 'dashboard', url: '/dashboard', component: 'appDashboard' },
   { name: 'detail', url: '/detail/:id', component: 'appDetail' },
-  { name: 'heroes', url: '/heroes', component: 'appHeroes'},
+  { name: 'heroes', url: '/heroes', component: 'appHeroes' },
 ];
